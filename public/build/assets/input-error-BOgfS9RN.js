78ab6174d004625e6886e70ec4942c5aefe9526d
Frankandev <fkamgang06@gmail.com>
2025-10-07 16:11:19 +0100

    deploy

---
+++ public/build/assets/input-error-BOgfS9RN.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-QMsShUqO.js";import{c as o}from"./button-DTQSF8DK.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
