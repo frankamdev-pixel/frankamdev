1df295b86274294a002d14f070af150d825311d0
frankamdev-pixel <frankamdev@gmail.com.com>
2025-10-14 21:56:31 -0700

    rgfsegr

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
   */
  public function register(): void
  {
-  //
+  if ($this->app->environment('local')) {
+   $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+  }
  }
 
  /**
